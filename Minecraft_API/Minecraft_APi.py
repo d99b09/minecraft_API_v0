9277fb7ffb17a931a017691b27361e62d056ca4c
dima <d99b09@gmail.com>
2022-06-29 19:24:37 +0300

    added: button api and mainloop

--- Minecraft_API/Minecraft_APi.py
+++ Minecraft_API/Minecraft_APi.py
@@ -1,6 +1,9 @@
+import os
+
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Controller as Controller2
-import pyautogui
+from pynput.keyboard import Key
+import asyncio
 
 import time
 from threading import Thread
@@ -15,24 +18,31 @@ class Minecraft_API_mio(Thread):
         self.duration = 0.001
         self.is_done = False
         self.pre_button_states = {'w': False, 'a': False, 's': False, 'd': False, 'e': False, 'shift': False,
-                              'ctrl': False, 'space': False}
+                              'ctrl': False, 'space': False, 'lCkick': False, 'rClick': False}
         self.button_states = {'w': False, 'a': False, 's': False, 'd': False, 'e': False, 'shift': False,
-                              'ctrl': False, 'space': False}
+                              'ctrl': False, 'space': False, 'lCkick': False, 'rClick': False}
 
-        self.button_headers = {}
+        self.button_keyboard_headers = {'w': 'w', 'a': 'a', 's': 's', 'd': 'd', 'e': 'e', 'shift': Key.shift,
+                                        'ctrl': Key.ctrl, 'space': Key.space}
+        self.button_mouse_headers = {'lCkick': Button.left, 'rClick': Button.right}
 
+    def minecraft_launcher_launch(self):
+        os.system("open /Users/dima/Downloads/TLauncher-2.86/TLauncher-2.86.jar")#mac os
 
 
     def run(self) -> None:
         while not self.is_done:
+            # print(self.x_speed, self.y_speed)
             self.mouse.move(self.x_speed, self.y_speed)
             self.keyboard_button_check_click()
             time.sleep(self.duration)
+            #time.sleep(0.1)
 
     def stop(self):
         self.is_done = True
 
     def rotationbyspeed(self, x, y):
+        print('rotation')
         if x != 0 and y != 0:
             min_speed = min(abs(x), abs(y))
         elif x != 0:
@@ -46,37 +56,64 @@ class Minecraft_API_mio(Thread):
         self.duration = 1/min_speed
 
     def keyboard_button_check_click(self):
+
         if self.pre_button_states != self.button_states:
+            print(self.pre_button_states)
 
             for e in self.pre_button_states:
                 if self.pre_button_states[e] != self.button_states[e]:
                     if self.button_states[e]:
-                        self.keyboard.press(e)
+                        if e in self.button_mouse_headers:
+                            self.mouse.press(self.button_mouse_headers[e])
+                        else:
+                            self.keyboard.press(self.button_keyboard_headers[e])
                     else:
-                        self.keyboard.release(e)
+                        if e in self.button_mouse_headers:
+                            self.mouse.release(self.button_mouse_headers[e])
+                        else:
+                            self.keyboard.release(self.button_keyboard_headers[e])
             self.pre_button_states = self.button_states.copy()
+            print(self.pre_button_states)
+
+
+    def press_button(self, button):
+        self.button_states[button] = True
+
+    def release_button(self, button):
+        self.button_states[button] = False
+
+
+
 
 
 
 def main():
     mapi = Minecraft_API_mio()
+    mapi.minecraft_launcher_launch()
+    return 0
     # mapi.button_click()
     mapi.start()
     time.sleep(3)
-    mapi.button_states['w'] = True
+    mapi.press_button('w')
+    # mapi.button_states['w'] = True
     # print(mapi.button_states)
     # print(mapi.pre_button_states)
 
-    # mapi.rotationbyspeed(100, 100)
+    #mapi.rotationbyspeed(100, 100)
+    # time.sleep(5)
+    # print(mapi.pre_button_states)
+    # mapi.button_states['w'] = False
+    # print(mapi.button_states)w
+    # print(mapi.pre_button_states)
     time.sleep(5)
+    mapi.release_button('w')
     print(mapi.pre_button_states)
-    mapi.button_states['w'] = False
     print(mapi.button_states)
-    print(mapi.pre_button_states)
-    time.sleep(5)
+    time.sleep(1)
+
     # mapi.button_states['w'] = True
     # time.sleep(5)
-    # mapi.button_states['w'] = False
+    mapi.button_states['w'] = False
 
     mapi.stop()
 
